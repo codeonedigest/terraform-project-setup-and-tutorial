b1fb70a0edc6dae7a9291262e451838e0b52864c
codeonedigest <codeonedigest@gmail.com>
2023-12-01 15:45:15 +0530

    Checking in first TF project

---
+++ Project1/provider.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.19.0" //https://registry.terraform.io/providers/hashicorp/aws/latest
+    }
+  }
+}
---
+++ Project1/terraform.tfvars
@@ -0,0 +1,4 @@
+ami           = "ami-0f74c08b8b5effa56"
+instance_type = "t2.micro"
+name_tag      = "My EC2 Instance"
+ 
